88f0a69f5e8f12edaa029553af1d5b7019cbed83
Tamás Nagy <tamas.nagy@apps.no>
2017-11-09 12:32:57 +0100

    Using more random data

--- producer/index.js
+++ producer/index.js
@@ -7,8 +7,14 @@ const REDIS_HASH_DONE = 'jobsdone'
 let timer
 
 const generateId = () => Math.round(Math.random() * 999999999)
-const generateData = (length) => new Array(length).fill(0).join('')
 const delayBetweenInserts = () => 1500 + (Math.random() * 1000)
+const generateRandomData = (length) => {
+  const arr = []
+  for (let i = 0; i < length; i++) {
+    arr.push(Math.round(Math.random() * 10))
+  }
+  return arr.join('')
+}
 
 const config = {
   redis: {
@@ -33,7 +39,7 @@ const pub = client.duplicate()
 const publishNewEntry = () => {
   const id = generateId()
   client.hset([REDIS_HASH_STATUS, id, JSON.stringify({ new: new Date })], () => {
-    client.rpush([REDIS_HASH_PROCESSABLE, JSON.stringify({ id: id, data: generateData(5000) })], () => {
+    client.rpush([REDIS_HASH_PROCESSABLE, JSON.stringify({ id: id, data: generateRandomData(512) })], () => {
       pub.publish(PUBSUB_CHANNEL_NAME, `A new job was inserted.`)
       console.log(new Date(), `A new job (#${id}) was inserted into ${REDIS_HASH_PROCESSABLE}`)
       timer = setTimeout(publishNewEntry, delayBetweenInserts())
